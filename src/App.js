e9a3e80e71a3385ebe7208cc8ccdcab4fcda16ef
Jonathan Funk <jonfunk88@hotmail.com>
2017-04-10 14:06:22 -0700

    on change handler made

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './styles/App.css';
 import Input from './components/input';
 
 class App extends Component {
-
-  handleSubmit(e){
+  constructor() {
+    super()
+    this.state = {
+      subreddit: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({subreddit: event.target.value});
+  }
+  handleSubmit = (e) => {
     e.preventDefault();
-		console.log('Some subreddit.')
-	}
-
+    console.log(this.state.subreddit)
+  }
   render() {
+
     return (
       <div className="App">
-        <Input clickSubmit={this.handleSubmit}/>
+        <Input 
+          clickSubmit={this.handleSubmit}
+          inputValue={this.handleChange}
+        />
         <section className="griddit-section">
         </section>
       </div>
--- src/components/input/index.js
+++ src/components/input/index.js
@@ -4,21 +4,22 @@ import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/RaisedButton';
 import styles from './mui-styles';
 
-const Input = ({clickSubmit}) => {
+const Input = ({ clickSubmit, inputValue }) => {
 	return (
 		<Paper style={styles.container}>
-			<form style={styles.form}>
+			<form style={styles.form} onSubmit={clickSubmit}>
 			<TextField
 				hintText="Type in your favorite subreddit here."
 				hintStyle={styles.hint}
 				style={styles.input}
+				onChange={inputValue}
     	/>
 			<FlatButton 
 				label="Submit"
+				type="submit"
 				backgroundColor='red'
 				style={styles.button} 
 				labelStyle={styles.overlay}
-				onClick={clickSubmit}
 			/>
 			</form>
 		</Paper>
