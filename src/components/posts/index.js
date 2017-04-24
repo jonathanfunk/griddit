09eb5d3322e245dd17009373df02ea83899dcc66
Jonathan Funk <jonfunk88@hotmail.com>
2017-04-23 19:57:04 -0700

    updated proptypes

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Card } from 'material-ui/Card';
 import styles from './styles';
